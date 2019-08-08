050f413bebf8db267b2e5a1a7ae3065b2e6eb2b8
sevincfurkan <sfurkan23@outlook.com>
2019-08-08 16:42:40 +0300

    Internship Project

--- src/main/java/com/sevincfurkan/airlineticketingsystem/util/DistanceCalculation.java
+++ src/main/java/com/sevincfurkan/airlineticketingsystem/util/DistanceCalculation.java
@@ -6,13 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 public class DistanceCalculation {
     @Autowired
-    private RotaServiceImpl rotaServiceImpl;
-    private Rota rota =new Rota();
+    private Rota rota=new Rota();
     private double distance(double originLat, double originLong, double destinationLat, double destinationLong) {
         double radius=6371;
-
         double dLat=deg2rad(destinationLat-originLat);
-        double dLong=deg2rad(destinationLong-originLong);
+        double dLong=deg2rad( destinationLong-originLong);
         double a=Math.sin(dLat/2) * Math.sin(dLat/2) +
                 Math.cos(deg2rad(originLat)) * Math.cos(deg2rad(destinationLat)) * Math.sin(dLong/2) * Math.sin(dLong/2);
         double c=2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
